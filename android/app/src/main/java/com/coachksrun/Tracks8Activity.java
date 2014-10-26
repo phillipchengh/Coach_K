824ec284228db7ce1a35c505ca4878e5dd3bd7ca
jztein <jiayuaw@gmail.com>
2014-10-26 02:51:01 -0700

    Get and display playlist choices

--- android/app/src/main/java/com/coachksrun/Tracks8Activity.java
+++ android/app/src/main/java/com/coachksrun/Tracks8Activity.java
@@ -1,11 +1,20 @@
 package com.coachksrun;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.AdapterView;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -16,11 +25,14 @@ import java.lang.StringBuffer;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Tracks8Activity extends Activity
 {
     public static String DEV_KEY = "afe6602f9dfd8d5552dfa555feda9fab0a0a3643";
-    public static String TEST_MIX_URL = "http://8tracks.com/mixes/14.json?api_key="+DEV_KEY;
+    public static String URL_HIP_HOP = "http://8tracks.com/mix_sets/tags:hip_hop:popular.json";
+    public static int CAP_NUM_MIXES = 3;
 
     private String readStream(InputStream is) {
         try {
@@ -41,8 +53,22 @@ public class Tracks8Activity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracks8);
-        Void[] babies = {};
-        (new TalkTo8Tracks()).execute(babies);
+
+        GetPlaylists();
+    }
+
+    private void GetPlaylists() {
+        // TODO(kristen): Let user choose genre. E.g. mix_%s_url % genre_tag?
+        URL url;
+        try {
+            url = new URL(URL_HIP_HOP);
+        }
+        catch (Exception e) {
+            System.err.println("Couldn't form url object: " + e.getMessage());
+            return;
+        }
+        URL[] urls = {url};
+        (new SetupPlaylists()).execute(urls);
     }
 
 
@@ -65,28 +91,93 @@ public class Tracks8Activity extends Activity
         return super.onOptionsItemSelected(item);
     }
 
-    private class TalkTo8Tracks extends AsyncTask<Void, Void, Void> {
-        protected Void doInBackground(Void... params) {
+    private HttpURLConnection SetUp8tracks(HttpURLConnection urlConnection) {
+        String user_agent = System.getProperty("http.agent");
+        try {
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("User-Agent", user_agent);
+            urlConnection.setRequestProperty("X-Api-Version", "2"); // No need user auth.
+            urlConnection.setRequestProperty("X-Api-Key", DEV_KEY);
+            urlConnection.setDoInput(true);
+            return urlConnection;
+        }
+        catch (Exception e) {
+            System.out.println("Couldn't set up url connection: " + e.getMessage());
+        }
+        return null;
+    }
+
+    private class SetupPlaylists extends AsyncTask<URL, Void, JSONObject> {
+        // NOTE: 8tracks calls playlists 'mixes'.
+
+        protected JSONObject doInBackground(URL... params) {
+            JSONObject json = null;
             try
             {
-                String user_agent = System.getProperty("http.agent");
-
-                URL url = new URL("http://www.android.com/");
+                URL url = params[0];
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setRequestProperty("User-Agent", user_agent);
-                urlConnection.setDoInput(true);
-
+                urlConnection = SetUp8tracks(urlConnection);
                 InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                System.out.println(readStream(in));
+                String response = readStream(in);
+
+                json = new JSONObject(response);
                 urlConnection.disconnect();
             }
             catch(Exception e)
             {
-                System.err.println("FAILUREB - Could not send HTTP GET Request: "+e.getMessage());
+                System.err.println("FAILURE - Could not send HTTP GET Request: " + e.getMessage());
                 e.printStackTrace();
             }
-            return null;
+            return json;
         }
+
+        // Handles get-playlists response.
+        protected void onPostExecute(JSONObject mixes_json) {
+            if (mixes_json == null) {
+                System.err.println("Couldn't get any mixes");
+                return;
+            }
+
+            // Get Ids and Names of mixes.
+            final Map<String, String> mix_id_names;
+            try {
+                JSONArray mixes = mixes_json.getJSONArray("mixes");
+                int number_of_mixes = mixes.length();
+                mix_id_names = new HashMap<String, String>();
+                for (int i = 0; i < number_of_mixes && i < CAP_NUM_MIXES; i++) {
+                    JSONObject mix = mixes.getJSONObject(i);
+                    mix_id_names.put(mix.getString("name"), mix.getString("id"));
+                }
+            } catch (Exception e) {
+                System.err.println("Malformed mixes json: " + mixes_json.toString());
+                return;
+            }
+
+            System.out.println(mix_id_names);
+
+            // Displays possible mix names to user.
+            // TODO(kristen): Change text color.
+            final String[] mix_names = mix_id_names.keySet().toArray(new String[0]);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    getApplicationContext(), android.R.layout.simple_list_item_1, mix_names);
+            ListView list_view = (ListView) findViewById(R.id.tracks8_playlists_selection_list);
+            list_view.setAdapter(adapter);
+
+            // Gets chosen mix with id for streaming.
+            AdapterView.OnItemClickListener mixClickedHandler = new AdapterView
+                    .OnItemClickListener() {
+                public void onItemClick(AdapterView parent, View v, int position, long id) {
+                    // User chose this; stream this mix!
+                    String chosen_mix_id = mix_id_names.get(mix_names[position]);
+                    System.out.println("Chosen id: " + chosen_mix_id);
+                }
+            };
+            list_view.setOnItemClickListener(mixClickedHandler);
+        }
+
+
     }
+
+
+
 }
